6c5f2c9868e3cc24c1a6bbc3eae8f317b3e660f4
Alejandro Garcia <agrcrobles@gmail.com>
2018-01-06 04:14:51 +0100

    Minor styles

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends React.Component<*, State> {
 				<View>
 					<View style={styles.banner}>
 						<Text style={styles.title}>Cool! everything is working fine!</Text>
-						<Text style={styles.subtitle}>Your ID is {this.state.id}</Text>
+						<Text style={styles.subtitle}>{this.state.id}</Text>
 					</View>
 					<Text style={[styles.info, styles.description]}>IPFS version</Text>
 					<Text style={styles.info}>{this.state.version}</Text>
